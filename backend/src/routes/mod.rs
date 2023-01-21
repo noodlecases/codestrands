af81e06c0dc4aee213bdb86d20a784c9e52b3672
Dennis Pham <dennis@dennispham.me>
2023-01-21 13:02:42 -0500

    Add user relationships routes.

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -3,10 +3,12 @@ use actix_web::web::ServiceConfig;
 mod auth;
 mod social_links;
 mod user;
+mod user_relationships;
 
 pub fn config(config: &mut ServiceConfig) {
     config
         .configure(auth::config)
         .configure(user::config)
+        .configure(user_relationships::config)
         .configure(social_links::config);
 }
