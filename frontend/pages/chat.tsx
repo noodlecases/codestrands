a9e6d01c4ecd44fce211ca950f5ff832cd3fd5e8
Aaron Ni <niaar2004@gmail.com>
2023-01-21 13:12:54 -0500

    updated chat layout

--- frontend/pages/chat.tsx
+++ frontend/pages/chat.tsx
@@ -7,28 +7,75 @@ const chat = () => {
         <div>
           <Navbar />
         </div>
-        <div className = "h-screen w-[50%] border-base-content border-x-2">
+        <div className = "h-screen w-[60%] border-base-content border-x-2">
             <div className='flex'>
-                <ul className="menu bg-base-300 w-56">
-                    <li><a>Item 1</a></li>
-                    <li><a>Item 2</a></li>
-                    <li><a>Item 3</a></li>
+                <ul className="menu bg-base-300 w-72">
+                    <li>
+                        <div className='flex'>
+                            <div className="chat-image avatar">
+                                <div className="w-8 h-8 mr-1 rounded-full">
+                                    <img src="https://placeimg.com/192/192/people" />
+                                </div>
+                                <a className='m-1'>Item 1</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className='flex'>
+                            <div className="chat-image avatar">
+                                <div className="w-8 h-8 mr-1 rounded-full">
+                                    <img src="https://placeimg.com/192/192/people" />
+                                </div>
+                                <a className='m-1'>Item 2</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className='flex'>
+                            <div className="chat-image avatar">
+                                <div className="w-8 h-8 mr-1 rounded-full">
+                                    <img src="https://placeimg.com/192/192/people" />
+                                </div>
+                                <a className='m-1'>Item 3</a>
+                            </div>
+                        </div>
+                    </li>
                 </ul>
                 <div className='h-screen w-full border-base-content border-l-2'>
                     <div className='flex justify-center h-12 m-4'>
                         Name
                     </div>
-                    <div className="chat chat-start">
-                        <div className="chat-bubble">It's over Anakin, <br/>I have the high ground.</div>
-                    </div>
-                    <div className="chat chat-end">
-                        <div className="chat-bubble">You underestimate my power!</div>
+                    <div className='m-4'>
+                        <div className="chat chat-start">
+                            <div className="chat-image avatar">
+                                <div className="w-10 rounded-full">
+                                <img src="https://placeimg.com/192/192/people" />
+                                </div>
+                            </div>
+                            <div className="chat-header">
+                                Obi-Wan Kenobi
+                                <time className="text-xs opacity-50">12:45</time>
+                            </div>
+                            <div className="chat-bubble">You were the Chosen One!</div>
+                        </div>
+                        <div className="chat chat-end">
+                            <div className="chat-image avatar">
+                                <div className="w-10 rounded-full">
+                                <img src="https://placeimg.com/192/192/people" />
+                                </div>
+                            </div>
+                            <div className="chat-header">
+                                Anakin
+                                <time className="text-xs opacity-50">12:46</time>
+                            </div>
+                            <div className="chat-bubble">I hate you!</div>
+                        </div>
                     </div>
 
                     <div className='absolute bottom-0'>
                         <div className="form-control">
                             <div className="input-group">
-                                <input type="text" placeholder="Send a message" className="input input-bordered" />
+                                <input type="text" placeholder="Send a message" className="input input-bordered w-96" />
                                 <button className="btn btn-square">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
@@ -39,9 +86,6 @@ const chat = () => {
                     </div>
                 </div>
             </div>
-        </div>
-        <div>
-          test
         </div>
     </div>
   )
