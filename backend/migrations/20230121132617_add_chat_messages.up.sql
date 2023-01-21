d983834cd3faacf8013ee7ca5b3461bd87f1823c
Dennis Pham <dennis@dennispham.me>
2023-01-21 08:30:37 -0500

    Add chat messages migration.

---
+++ backend/migrations/20230121132617_add_chat_messages.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE chat_messages (
+    id serial NOT NULL,
+    chat_id integer NOT NULL,
+    user_id integer NOT NULL,
+    created_at timestamptz DEFAULT NOW() NOT NULL,
+
+    PRIMARY KEY (id),
+    UNIQUE (chat_id, user_id),
+    FOREIGN KEY (chat_id) REFERENCES chats(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
