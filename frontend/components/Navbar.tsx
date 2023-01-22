89789563c63324e1387bfa85a389a7e6ddef995a
Aaron Ni <niaar2004@gmail.com>
2023-01-21 20:51:16 -0500

    added navbar links

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -6,13 +6,13 @@ const Navbar = () => {
     <ul className="menu bg-none w-56 p-2 rounded-box">
         <Image src="/../public/assets/logo.png" alt="logo" width={80} height={80} className="p-4"/>
         <li>
-            <a>
+            <a href="/swipe">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
             Home
             </a>
         </li>
         <li>
-            <a>
+            <a href="/chat">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
             <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
             </svg>
@@ -20,7 +20,7 @@ const Navbar = () => {
             </a>
         </li>
         <li>
-            <a>
+            <a  href="/profile">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
             <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
             </svg>
--- frontend/pages/profile.tsx
+++ frontend/pages/profile.tsx
@@ -15,9 +15,9 @@ export const profile = () => {
   return (
     <div className = "flex justify-center">
         <div className='flex justify-end w-20 p-2'>
-            <button className="btn btn-circle btn-outline">
+            <a className="btn btn-circle btn-outline" href="/swipe">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
-            </button>
+            </a>
         </div>
         <div className = "flex h-screen w-[60%] border-base-content border-x-2"> 
             <ProfileExpanded  {...user}/>
